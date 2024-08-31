f03920fc5cdd7046a25c36198b89f4685b2be8b9
awaisamjad <awaisamjad08@gmail.com>
2024-08-31 17:16:47 +0100

    added an improved footer and navbar. created a page for the cms

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,51 +2,154 @@
 	import '../app.css';
 </script>
 
-<nav class="flex flex-row bg-slate-300 p-2">
-    <div class="flex items-center justify-center">
-        <a href="/">LOGO</a>
-    </div>
-    <div class="flex-grow flex justify-end">
-        <a href="/about">About</a>
-        <a href="/account">Account</a>
-        <a href="/ClothingItemPage">Clothes</a>
-    </div>
+<nav class="bg-gray-800">
+	<div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+		<div class="relative flex h-16 items-center justify-between">
+			<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+				<!-- Mobile menu button-->
+				<button
+					type="button"
+					class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+					aria-controls="mobile-menu"
+					aria-expanded="false"
+				>
+					<span class="absolute -inset-0.5"></span>
+					<span class="sr-only">Open main menu</span>
+					<!--
+              Icon when menu is closed.
+  
+              Menu open: "hidden", Menu closed: "block"
+            -->
+					<svg
+						class="block h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+						/>
+					</svg>
+					<!--
+              Icon when menu is open.
+  
+              Menu open: "block", Menu closed: "hidden"
+            -->
+					<svg
+						class="hidden h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			</div>
+			<div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+				<div class="flex flex-shrink-0 items-center">
+					<a href="/">
+						<img
+							class="h-8 w-auto"
+							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+							alt="Your Company"
+						/>
+					</a>
+				</div>
+				<div class="hidden sm:ml-6 sm:block">
+					<div class="flex space-x-4">
+						<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+						<a
+							href="/about"
+							class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white"
+							aria-current="page">About</a
+						>
+						<a
+							href="/account"
+							class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+							>Account</a
+						>
+						<a
+							href="/ClothingItemPage"
+							class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+							>Clothes</a
+						>
+					</div>
+				</div>
+			</div>
+			<div
+				class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+			>
+				<button
+					type="button"
+					class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+				>
+					<span class="absolute -inset-1.5"></span>
+					<span class="sr-only">View notifications</span>
+					<svg
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						aria-hidden="true"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+						/>
+					</svg>
+				</button>
+			</div>
+		</div>
+	</div>
+
+	<!-- Mobile menu, show/hide based on menu state. -->
+	<div class="sm:hidden" id="mobile-menu">
+		<div class="space-y-1 px-2 pb-3 pt-2">
+			<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+			<a
+				href="/about"
+				class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+				aria-current="page">About</a
+			>
+			<a
+				href="/account"
+				class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+				>Account</a
+			>
+			<a
+				href="/ClothingItemPage"
+				class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+				>Clothes</a
+			>
+		</div>
+	</div>
 </nav>
 
 <slot />
-<div class = " p-96">
-
-</div>
+<div class=" p-96"></div>
 <!-- Footer -->
-<div class=" bg-gray-900  ">
-    <div class="max-w-2xl mx-auto text-white py-10">
-        <div class="text-center">
-            <h3 class="text-3xl mb-3"> Download our fitness app </h3>
-            <p> Stay fit. All day, every day. </p>
-            <div class="flex justify-center my-10">
-                <div class="flex items-center border rounded-lg px-4 py-2 w-52 mx-2">
-                    <img src="https://cdn-icons-png.flaticon.com/512/888/888857.png" alt="play store" class="w-7 md:w-8">
-                    <div class="text-left ml-3">
-                        <p class='text-xs text-gray-200'>Download on </p>
-                        <p class="text-sm md:text-base"> Google Play Store </p>
-                    </div>
-                </div>
-                <div class="flex items-center border rounded-lg px-4 py-2 w-44 mx-2">
-                    <img src="https://cdn-icons-png.flaticon.com/512/888/888841.png" alt="apple store" class="w-7 md:w-8">
-                    <div class="text-left ml-3">
-                        <p class='text-xs text-gray-200'>Download on </p>
-                        <p class="text-sm md:text-base"> Apple Store </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="mt-28 flex flex-col md:flex-row md:justify-between items-center text-sm text-gray-400">
-            <p class="order-2 md:order-1 mt-8 md:mt-0"> &copy; Beautiful Footer, 2021. </p>
-            <div class="order-1 md:order-2">
-                <span class="px-2">About us</span>
-                <span class="px-2 border-l">Contact us</span>
-                <span class="px-2 border-l">Privacy Policy</span>
-            </div>
-        </div>
-    </div>
-</div>
+<footer class="bg-gray-800">
+	<div class="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+		<div class="sm:flex sm:items-center sm:justify-between">
+			<div class="flex justify-center text-teal-600 sm:justify-start">
+				<img
+					class="h-8 w-auto"
+					src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+					alt="Your Company"
+				/>
+			</div>
+
+			<p class="mt-4 text-center text-sm text-gray-500 lg:mt-0 lg:text-right">
+				Copyright &copy; 2022. All rights reserved.
+			</p>
+		</div>
+	</div>
+</footer>
--- src/routes/account/+page.svelte
+++ src/routes/account/+page.svelte
@@ -1,35 +1,49 @@
 <script>
-    let file = '';
-    let number = '';
-    let text = '';
-
-    function handleSubmit() {
-        // Handle form submission here
-        console.log("File:", file);
-        console.log("Number:", number);
-        console.log("Text:", text);
+	import { goto } from '$app/navigation';
+  let username = '';
+  let password = '';
+  
+  function authenticate() {
+    if (username == "Awais" && password == "Amjad"){
+      goto('/cms')
     }
+    else{
+      alert("Unsuccessful")
+    }
+  }
+  
 </script>
 
 <div class="w-full max-w-xs">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-          Name
-        </label>
-        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username">
-      </div>
-      <div class="mb-6">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-          Price
-        </label>
-        <input class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="number">
-        <p class="text-red-500 text-xs italic">Please choose a password.</p>
-      </div>
-      <div class="flex items-center justify-between">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-          Sign In
-        </button>
-      </div>
-    </form>
-  </div>
+	<form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+		<div class="mb-4">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="username"> Name </label>
+			<input
+				class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+				id="username"
+				type="text"
+				placeholder="Username"
+        bind:value={username}
+			/>
+		</div>
+		<div class="mb-6">
+			<label class="block text-gray-700 text-sm font-bold mb-2" for="password"> Password </label>
+			<input
+				class="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+				id="password"
+				type="text"
+        bind:value={password}
+			/>
+			<p class="text-red-500 text-xs italic">Please choose a password.</p>
+		</div>
+		<div class="flex items-center justify-between">
+			<button
+				class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+				type="button"
+				on:click={authenticate}
+			>
+				Sign In
+			</button>
+		</div>
+	</form>
+</div>
